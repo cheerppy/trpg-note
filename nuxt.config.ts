62cceedf2850b6e4cdd382ba430ba831f0b24698
cheerppy <john0217smith@gmail.com>
2023-05-15 06:30:26 +0900

    remove baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        baseURL: "/trpg-note/"
-    },
     css: [
         'assets/common.scss'
     ],
