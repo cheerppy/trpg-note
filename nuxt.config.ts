a2c85bb511c0721be5898231f71b1f6e9c32bf8a
cheerppy <john0217smith@gmail.com>
2023-05-25 07:37:26 +0900

    trial: scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: [
-        'assets/common.scss'
+        '~/assets/common.scss'
     ],
     vite: {
         css: {
